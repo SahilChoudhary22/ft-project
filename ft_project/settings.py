d631329e987b5088b7377c2cee9cb8a8c470c3cc
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-05-31 14:53:34 +0530

    deploying

--- ft_project/settings.py
+++ ft_project/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATIC_URL = '/static/'
 
